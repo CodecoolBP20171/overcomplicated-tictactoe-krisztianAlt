ffd0fcd61ddc23f02e388b11ac822ec65383321d
krisztian <alt.krisztian@gmail.com>
2017-12-22 11:59:58 +0100

    Implement winning, defeat and draw

--- overcomplicated-tictactoe/src/main/java/com/codecool/enterprise/overcomplicated/controller/GameController.java
+++ overcomplicated-tictactoe/src/main/java/com/codecool/enterprise/overcomplicated/controller/GameController.java
@@ -2,11 +2,13 @@ package com.codecool.enterprise.overcomplicated.controller;
 
 import com.codecool.enterprise.overcomplicated.model.Player;
 import com.codecool.enterprise.overcomplicated.model.TictactoeGame;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @SessionAttributes({"player", "game"})
@@ -15,6 +17,7 @@ public class GameController {
     @Autowired
     protected RestTemplate restTemplate;
 
+    private org.slf4j.Logger logger = LoggerFactory.getLogger(GameController.class);
 
     @ModelAttribute("player")
     public Player getPlayer() {
@@ -52,11 +55,43 @@ public class GameController {
     @GetMapping(value = "/game-move")
     public String gameMove(@ModelAttribute("player") Player player,
                            @ModelAttribute("move") int move,
-                           @ModelAttribute("game") TictactoeGame tictactoeGame) {
+                           @ModelAttribute("game") TictactoeGame tictactoeGame,
+                           RedirectAttributes redirectAttributes) {
         System.out.println("Player moved " + move);
-        tictactoeGame.setAGameField(move, "O");
-        int computerStep = restTemplate.getForObject("http://localhost:60001" + "/{gameStatus}", int.class, tictactoeGame.getGameFields());
-        tictactoeGame.setAGameField(computerStep, "X");
+
+        if (tictactoeGame.isGameClosed()){
+            redirectAttributes.addFlashAttribute("gameover", "Game over, you cannot select another field, please, start new game.");
+        } else {
+            boolean fieldIsReserved = tictactoeGame.checkFieldIsEmpty(move);
+
+            if (fieldIsReserved){
+                redirectAttributes.addFlashAttribute("error", "Field is 0reserved, please, click another one!");
+            } else {
+                tictactoeGame.setAGameField(move, "O");
+
+                if (tictactoeGame.draw()){
+                    redirectAttributes.addFlashAttribute("gameover", "Draw. Keep breath and try again.");
+                } else {
+                    if (tictactoeGame.playerWins()){
+                        redirectAttributes.addFlashAttribute("gameover", "Glorious victory!");
+                    } else {
+                        int computerStep = restTemplate.getForObject("http://localhost:60001" + "/{gameStatus}", int.class, tictactoeGame.getGameFields());
+
+                        if (computerStep != -1){
+                            tictactoeGame.setAGameField(computerStep, "X");
+                            if (tictactoeGame.computerWins()){
+                                redirectAttributes.addFlashAttribute("gameover", "Defeat. Try again, we trust you.");
+                            } else if (tictactoeGame.draw()){
+                                redirectAttributes.addFlashAttribute("gameover", "Draw. Keep breath and try again.");
+                            }
+                        } else {
+                            logger.error("DATA REQUEST FAILED. READ AI LOG MESSAGE!");
+                        }
+                    }
+                }
+            }
+        }
+
         return "redirect:/game";
     }
 
--- overcomplicated-tictactoe/src/main/java/com/codecool/enterprise/overcomplicated/model/TictactoeGame.java
+++ overcomplicated-tictactoe/src/main/java/com/codecool/enterprise/overcomplicated/model/TictactoeGame.java
@@ -4,16 +4,18 @@ public class TictactoeGame {
 
     private String gameFields = "---------";
 
+    private boolean gameClosed = false;
+
     public String getGameFields() {
         return gameFields;
     }
 
-    public void setGameFields(String gameFields) {
-        this.gameFields = gameFields;
+    public boolean isGameClosed() {
+        return gameClosed;
     }
 
-    public String getAGameField(int index) {
-        return gameFields.substring(index, index+1);
+    public void setGameClosed(boolean gameClosed) {
+        this.gameClosed = gameClosed;
     }
 
     public void setAGameField(int index, String playerSign) {
@@ -22,5 +24,65 @@ public class TictactoeGame {
                     gameFields.substring(index+1, gameFields.length());
     }
 
+    public boolean checkFieldIsEmpty(int index) {
+        if (gameFields.substring(index, index + 1).equals("-")){
+            return false;
+        }
+        return true;
+    }
 
+    public boolean playerWins() {
+        return checkWin("O");
+    }
+
+    public boolean computerWins() {
+        return checkWin("X");
+    }
+
+    private boolean checkWin(String sign){
+        boolean winning = false;
+        String winningRow = sign + sign + sign;
+        if (gameFields.substring(0, 3).equals(winningRow)){
+            winning = true;
+        } else if (gameFields.substring(3, 6).equals(winningRow)){
+            winning = true;
+        } else if (gameFields.substring(6, 9).equals(winningRow)){
+            winning = true;
+        } else if (gameFields.substring(0, 1).equals(sign) &&
+                gameFields.substring(3, 4).equals(sign) &&
+                gameFields.substring(6, 7).equals(sign)){
+            winning = true;
+        } else if (gameFields.substring(1, 2).equals(sign) &&
+                gameFields.substring(4, 5).equals(sign) &&
+                gameFields.substring(7, 8).equals(sign)){
+            winning = true;
+        } else if (gameFields.substring(2, 3).equals(sign) &&
+                gameFields.substring(5, 6).equals(sign) &&
+                gameFields.substring(8, 9).equals(sign)){
+            winning = true;
+        } else if (gameFields.substring(0, 1).equals(sign) &&
+                gameFields.substring(4, 5).equals(sign) &&
+                gameFields.substring(8, 9).equals(sign)){
+            winning = true;
+        } else if (gameFields.substring(2, 3).equals(sign) &&
+                gameFields.substring(4, 5).equals(sign) &&
+                gameFields.substring(6, 7).equals(sign)){
+            winning = true;
+        }
+
+        if (winning){
+            gameClosed = true;
+        }
+
+        return winning;
+    }
+
+    public boolean draw() {
+        if (gameFields.contains("-")){
+            return false;
+        }
+
+        gameClosed = true;
+        return true;
+    }
 }
